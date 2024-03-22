431a4084438bd42a05bf16bea7f2a52c7b896283
Hanbit <ghiesf@naver.com>
2024-03-22 11:05:44 +0900

    Test: feature 브랜치에서 테스트 코드 수정

--- src/main/java/com/example/demo/repository/TestRepository.java
+++ src/main/java/com/example/demo/repository/TestRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
 public class TestRepository {
+    public static void main(String[] args) {
+        System.out.println("나를 추가해보렴!!!!!");
+
+        // 코드 변경사항 가정 (테스트 코드 삭제)
+    }
 }
